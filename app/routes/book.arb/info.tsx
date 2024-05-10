231830e9fb370c4931c44f3d0aaed7883050a63a
wrule <1982775886@qq.com>
2024-05-11 00:30:04 +0800

    文件夹理解

---
+++ app/routes/book.arb/info.tsx
@@ -0,0 +1,5 @@
+
+export default
+function Info() {
+  return <div>Arb是目前用户最多的Layer2</div>;
+}
---
+++ app/routes/book.arb/route.tsx
@@ -0,0 +1,9 @@
+import Info from './info';
+
+export default
+function BookARB() {
+  return <div>
+    <div>Arb协议</div>
+    <div><Info /></div>
+  </div>;
+}
